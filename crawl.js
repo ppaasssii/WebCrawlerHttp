86768b79fdc17475bc90a1fd030fc729d5313c61
ppaasssii <pascal.knoller@gmail.com>
2024-10-26 22:13:07 +0200

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,18 +1,47 @@
-function normalizeUrl(urlString){
-    // URL is a built-in class in Node.js
-    const urlObj = new URL(urlString)
-    // hostname is the domain name, pathname is the path after the domain name
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
+const {JSDOM} = require('jsdom')
 
-    // if the last character is a slash '/', remove
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/'){
-        // slice(0, -1) means everything except the last character
-       return hostPath.slice(0, -1)
+
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody) // JSDOM is a library that allows us to parse HTML
+    const linkElements = dom.window.document.querySelectorAll('a') // querySelectorAll returns a NodeList, which is similar to an array
+
+    for ( const linkElement of linkElements){  // iterate over the NodeList and extract the href attribute
+        if (linkElement.href.startsWith('/')){
+            //relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`)  // concatenate the base URL with the relative path
+                urls.push(urlObj.href)
+            } catch (error){
+                console.log(`Error Invalid relative URL: ${error.message}`)
+            }
+        } else {
+            //absolute
+            try {
+                const urlObj = new URL(linkElement.href) // concatenate the base URL with the relative path
+                urls.push(urlObj.href)
+            } catch (error){
+                console.log(`Error Invalid absolute URL: ${error.message}`)
+            }
+        }
+    }
+    return urls
+}
+
+
+
+function normalizeUrl(urlString){
+    const urlObj = new URL(urlString) // URL is a built-in class in Node.js that allows us to parse URLs
+    const hostPath = `${urlObj.hostname}${urlObj.pathname}`// hostname is the domain name, pathname is the path after the domain name
+
+    if (hostPath.length > 0 && hostPath.endsWith('/')){     // if the last character is a slash '/', remove
+       return hostPath.slice(0, -1) // slice(0, -1) means everything except the last character
     }
     else
         return hostPath
 }
 
 module.exports = {
-    normalizeUrl
+    normalizeUrl,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeUrl} = require('./crawl.js');
+const {normalizeUrl, getURLsFromHTML} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
 
 
@@ -27,5 +27,80 @@ test ('normalizeURL strip http', () => {
     const input = 'http://blog.boot.dev/path/'
     const actual = normalizeUrl(input)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test ('getURLsFromHTML absolute', () => {
+    // HTML body with a single link to the blog homepage
+    const inputHTMLBody = `
+<html>
+    <body>
+    
+        <a href="https://blog.boot.dev/"> 
+            Boot.dev Blog
+        </a>
+    </body>
+</html>   
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/"]
+    expect(actual).toEqual(expected)
+})
+
+test ('getURLsFromHTML relative', () => {
+    // HTML body with a single link to the blog homepage
+    const inputHTMLBody = `
+<html>
+    <body>
+    
+        <a href="/path/"> 
+            Boot.dev Blog
+        </a>
+    </body>
+</html>   
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test ('getURLsFromHTML absolute and relative', () => {
+
+    // HTML body with a single link to the blog homepage
+    const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://blog.boot.dev/path1/"> //absolute
+            Boot.dev Blog Path One
+        </a>
+        <a href="/path2/"> //relative
+            Boot.dev Blog Two
+        </a>
+    </body>
+</html>   
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/", "https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test ('getURLsFromHTML invalid', () => {
+    // HTML body with a single link to the blog homepage
+    const inputHTMLBody = `
+<html>
+    <body>
+    
+        <a href="invalid"> 
+            Invalid URL
+        </a>
+    </body>
+</html>   
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
