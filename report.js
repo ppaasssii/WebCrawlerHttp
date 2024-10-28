4f2309a08a5febc4f6562ce68d0a3a2a19f2d569
ppaasssii <pascal.knoller@gmail.com>
2024-10-28 18:42:24 +0100

    File name adapted: Timestamp of export & Website of crawl

--- report.js
+++ report.js
@@ -18,9 +18,6 @@ function printAndSaveReport(pages) {
         const hits = sortedPage[1];
         console.log(`Found ${hits} links to page: ${url}`);
     }
-    console.log('================');
-    console.log('REPORT END');
-    console.log('================');
 
     const folderPath = path.join(os.homedir(), 'Documents', 'WebCrawlerReports'); // create a folder "WebCrawler Reports" under downloads if it does not exist and save the file there
     try {
@@ -31,10 +28,19 @@ function printAndSaveReport(pages) {
             throw error;
         }
     }
+
     const worksheet = xlsx.utils.aoa_to_sheet([['URL', 'Hits'], ...sortedPages]); // create a worksheet
     const workbook = xlsx.utils.book_new(); // create a new workbook
+    const baseURL = process.argv[2];
+    const reportTS = new Date().toISOString(); // create a timestamp for the report
+   const fileName = `${reportTS}_${baseURL.replace(/https?:\/\//, '').replace(/[^a-zA-Z0-9]/g, '')}_report.xlsx`;
     xlsx.utils.book_append_sheet(workbook, worksheet, 'Report');
-    xlsx.writeFile(workbook, `${folderPath}/report.xlsx`);
+    xlsx.writeFile(workbook, `${folderPath}/${fileName}`);
+
+    console.log('================');
+    console.log(`Report saved to: ${folderPath}/${fileName}`);
+    console.log('REPORT END');
+    console.log('================');
 }
 
 /**
